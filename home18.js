48e12526b2d5d21b0a87b58b2c5d157181c27cf3
makhset <beibarysm10@gmail.com>
2024-11-19 16:08:24 +0500

    Add files via upload

---
+++ home18.js
@@ -0,0 +1,8 @@
+function compareWords(word1, word2) {
+    if (word1.toLowerCase() === word2.toLowerCase()) {
+      console.log("Сөздер бірдей");
+    } else {
+      console.log("Сөздер әртүрлі");
+    }
+  }
+  
